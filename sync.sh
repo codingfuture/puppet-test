4109df263bfec03bd0e1a73b641f0ca9a828dfcb
Andrey Galkin <andrey@futoin.org>
2016-02-24 04:46:35 +0200

    Saving work on secondary master support

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 vagrant rsync puppet
 vagrant rsync puppetback
 
-for h in maint router puppet db web; do
+for h in maint router puppet db puppetback web; do
         echo "Provisioning $h"
         #vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppet.example.com ip=10.10.1.11
         #vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host maint.example.com ip=10.10.1.10
