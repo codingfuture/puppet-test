5b06723d1f153387098bb04ec7ea89c3ac686f94
Andrey Galkin <andrey@futoin.org>
2016-05-22 07:24:54 +0300

    Added garbd tests

--- sync.sh
+++ sync.sh
@@ -7,5 +7,5 @@ for h in maint router puppet db puppetback web dbclust1 dbclust2; do
         echo "Provisioning $h"
         #vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppet.example.com ip=10.10.1.11
         #vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host maint.example.com ip=10.10.1.10
-        vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet agent --test
+        vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet agent --test --trace
 done
