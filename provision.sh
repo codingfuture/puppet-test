65f0c4b4b2e9bee49fd36f7399b5445413e6b1ed
Andrey Galkin <andrey@futoin.org>
2016-06-05 05:21:22 +0300

    Updated provisioning to better match current state

--- provision.sh
+++ provision.sh
@@ -2,15 +2,31 @@
 
 test ! -d modules && librarian-puppet install
 vagrant up
+
+skip_dns=${skip_dns:-0}
+
 #vagrant ssh puppet -- sudo service puppetdb restart
 #vagrant ssh puppet -- sudo service puppetserver restart
-for h in maint router puppet puppetback db web dbclust1 dbclust2; do
-        echo "Provisioning $h"
-        vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host maint.example.com ip=10.10.1.10
-        vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppet.example.com ip=10.10.1.11
-        vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppetback.example.com ip=10.10.1.12
-        
-	while ! vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet agent --test; do
-            sleep 1;
-	done
+for i in 1 2 3 4; do
+    for h in maint router puppet puppetback db dbclust1 dbclust2 web; do
+            echo "Provisioning $h"
+            
+            if test "$skip_dns" != 1; then
+                if test $h = 'maint';  then
+                    vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host maint.example.com ip=10.10.1.10
+                    vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppet.example.com ip=10.10.1.11
+                    vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet resource host puppetback.example.com ip=10.10.1.12
+                fi
+                vagrant ssh $h -c "echo 'nameserver 10.10.1.10' | sudo tee /etc/resolv.conf"
+            fi
+            
+            vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet agent --test --trace
+            vagrant ssh maint -- sudo /opt/puppetlabs/bin/puppet agent --test --trace
+            
+            while ! vagrant ssh $h -- sudo /opt/puppetlabs/bin/puppet agent --test --trace; do
+                sleep 1;
+            done
+    done
+    
+    skip_dns=1
 done
