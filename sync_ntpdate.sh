3bf6721aab1d8379f1a6228388a90c4f66fb2d99
Andrey Galkin <andrey@futoin.org>
2017-02-10 20:04:29 +0200

    Fixed to call cf_ntpdate for sync

--- sync_ntpdate.sh
+++ sync_ntpdate.sh
@@ -2,5 +2,5 @@
 
 for h in maint router puppet puppetback dbclust1 dbclust2 db web web2 web3; do
         echo "Syncing date on $h"
-        vagrant ssh $h -- sudo ntpdate-debian -u
+        vagrant ssh $h -- sudo cf_ntpdate
 done
