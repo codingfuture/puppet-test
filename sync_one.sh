ac278b27d71f8d1b12311ad989d98c28cfb71a62
Andrey Galkin <andrey@futoin.org>
2017-10-14 03:51:45 +0300

    Completed cfweb demo sites

---
+++ sync_one.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+source provision_common.sh
+set +e
+
+vagrant_rsync puppet
+vagrant_rsync puppetback
+
+for h in $@; do
+        echo "Provisioning $h"
+        puppet_deploy $h
+done
